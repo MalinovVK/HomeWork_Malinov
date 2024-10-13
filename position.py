923e7e110bbe2fd417f0acba8ed889330b6c6b4a
MalinovVK <malinov.vk@mail.ru>
2024-10-13 20:58:40 +0300

    Add files via upload

---
+++ position.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    i = 0
+    for string in strings:
+        i += 1
+        strings_positions[i, file.tell()] = string
+        file.write(f'{string}\n')
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
