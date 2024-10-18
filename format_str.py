94db5d3893821f90cfffbfd824717f9a64dcf5c1
MalinovVK <malinov.vk@mail.ru>
2024-10-18 12:27:17 +0300

    Add files via upload

---
+++ format_str.py
@@ -0,0 +1,19 @@
+team1_num = 4
+print('В команде Мастера кода участников: %s!' %team1_num)
+team2_num = 8
+print('В команде Мастера кода участников: %s и %s!\n' %(team1_num, team2_num))
+
+score_2 = 52
+print('Команда Волшебники данных решила задач: {}!'.format(score_2))
+team1_time = 183293
+print('Команда Волшебники данных решили задачи за: {} с!\n'.format(team1_time))
+
+score_1 = 14
+score_2 = 53
+print(f'Команды решили {score_1} и {score_2} задач')
+challenge_result = 'Мастер кода'
+print(f'Результат битвы: победа команды {challenge_result}!')
+
+tasks_total = 34
+time_avg = 443
+print(f'Сегодня было решено {tasks_total}, в среднем по {time_avg} секунды на задачу!')
