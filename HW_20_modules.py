510c595c2a75c565e8b22583eddb1ffd3b3849b9
MalinovVK <malinov.vk@mail.ru>
2024-10-23 13:09:03 +0300

    Delete HW_20_modules.py

--- HW_20_modules.py
+++
@@ -1,7 +0,0 @@
-from fake_math import divide as divide_fake
-from true_math import divide as divide_true
-print(divide_fake(200, 0))
-print(divide_fake(200, 20))
-print('')
-print(divide_true(200, 0))
-print(divide_true(200, 25))
