7f0bc5a4a373d96965b54e3c85f590f5ad383777
MalinovVK <malinov.vk@mail.ru>
2024-12-28 00:08:27 +1000

    my_bottt.py

--- my_bottt.py
+++ my_bottt.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7931711383:AAE1jf7EhEVROJ1zKAPF42NgyUm3jftEEZ0"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
