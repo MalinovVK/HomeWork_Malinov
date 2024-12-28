604606c4b343d882fb8e5d2373c87270eea4db09
MalinovVK <malinov.vk@mail.ru>
2024-12-29 00:26:41 +1000

    my_bottt.py

--- my_bottt.py
+++ my_bottt.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = "7931711383:AAE1jf7EhEVROJ1zKAPF42NgyUm3jftEEZ0"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
