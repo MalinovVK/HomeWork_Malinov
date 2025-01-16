4d40e656102f2603c9e2ba23005d74bf1660eb56
MalinovVK <malinov.vk@mail.ru>
2025-01-16 10:51:40 +0300

    Add files via upload

---
+++ db2.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL, 
+age INTEGER, 
+balance INTEGER NOT NULL)
+''')
+
+cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+# for i in range(1, 11):
+#     cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)", (f"User{i}", f"email_example{i}", f"{i*10}", f"1000"))
+# for i in range(1, 11, 2):
+#     cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
+# cursor.execute("DROP TABLE Users")
+# for i in range(1, 11, 3):
+#     cursor.execute(f"DELETE FROM Users WHERE id = {i}")
+# cursor.execute("DELETE FROM Users WHERE id = 6")
+cursor.execute("SELECT COUNT(*) FROM Users")
+count = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+total_balance = cursor.fetchone()[0]
+print(total_balance/count)
+connection.close()
